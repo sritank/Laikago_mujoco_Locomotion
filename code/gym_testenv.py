aa5e4b2c0cc6ebc06fbaef668124efa0171aad2a
Srinath <sritank@github.com>
2021-01-25 00:24:46 -0600

    updated laikago and quad_gym environments

--- code/gym_testenv.py
+++ code/gym_testenv.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-qpos0_hist=np.ones((1,106))
+# qpos0_hist=np.ones((1,109))
+qpos0_hist=np.ones((1,137))
+
 # qpos0_hist=np.ones((1,29))
 # qpos0_hist=np.ones((1,17))
 i=0
 env = gym.make('QuadGym-v2')
-# env = gym.make('HalfCheetah-v2')
+
 
 for i_episode in range(2):
     observation = env.reset()
-    for t in range(0,1000):
+    for t in range(0,2000):
         env.render()
         #printing the observation space
         # print(observation) 
         #storing the number of possible actions
         # ipdb.set_trace()
-        action = np.zeros(8)#env.action_space.sample() 
+        action = np.zeros(12)#env.action_space.sample() 
         # action = np.zeros(2)
         # action = np.zeros(6)
         # action[2]= 35 + math.sin(t/50)* 10 
@@ -28,48 +30,35 @@ for i_episode in range(2):
         # action[8]= 30 + math.cos(t/50)*8
         # action[11]= 30 + math.cos(t/50)*8
 
-        # action[2]= 3*math.sin(t/30)
-        # action[5]= 3*math.sin(t/30)
-        # action[8]= 3*math.cos(t/30)
-        # action[11]= 3*math.cos(t/30)
+        action[2]= math.sin(t/30)
+        action[5]= math.sin(t/30)
+        action[8]= math.cos(t/30)
+        action[11]= math.cos(t/30)
 
-        action[1]= 1*math.sin(t/50)
-        action[3]= 1*math.sin(t/50)
-        action[5]= 1*math.cos(t/50)
-        action[7]= 1*math.cos(t/50)
+        # action[2]= -2#*math.sin(t/20)
+        # action[5]= -2#*math.sin(t/20)
+        # action[8]= -2#*math.cos(t/20)
+        # action[11]= -2#*math.cos(t/20)
 
-        # action[1]= -1
-        # action[3]= -1
-        # action[5]= -1
-        # action[7]= -1
-        # action[0] = 1*math.cos(t/50)
-        # action[1] = 1*math.sin(t/50)
-        # action[0] = 1*math.cos(t/15)
-        # action[1] = 1*math.sin(t/15)
-        # action[0] = 1
-        # action[1] = 1
+        action[1]= math.sin(t/20)
+        action[4]= math.sin(t/20)
+        action[7]= math.cos(t/20)
+        action[10]= math.cos(t/20)
 
-        # action[1]= .5
-        # action[3]= .5
-        # action[5]= .5
-        # action[7]= .5
+        # action[1]= -9#*(50-15*math.sin(t/20))
+        # action[4]= -9#*(50-15*math.sin(t/20))
+        # action[7]= 7#+15*math.cos(t/20)
+        # action[10]= 7#+15*math.cos(t/20)
 
-
-
-        # action[1]= 25 #+ math.sin(t/50)* 10
-        # action[4]= 25 #+ math.sin(t/50)*10
-        # action[7]= 10 #+math.sin(t/50)*10
-        # action[10]= 10 #+math.sin(t/50)*10
-
-        # action[0]= -1#+math.cos(t/150)* 20
-        # action[3]= 1#+math.cos(t/150)*20
-        # action[6]= -1#+math.cos(t/150)*20
-        # action[9]= 1#+math.cos(t/150)*20
+        action[0]= 0#-5*math.sin(t/80)
+        action[3]= 0#+5*math.sin(t/80)
+        action[6]= 0#-5-2*math.sin(t/20)
+        action[9]= 0#5+2*math.sin(t/20)
 
         observation, reward, done, info = env.step(action)
         
         # qpos0_hist = np.append(qpos0_hist,reward)
-        qpos0_hist = np.vstack((qpos0_hist,observation))
+        # qpos0_hist = np.vstack((qpos0_hist,observation))
         # if t%10==0:
         #     print(info)
         # if done:
@@ -84,6 +73,6 @@ env.close()
 
 qpos0_hist = np.delete(qpos0_hist,0,0)
 
-plt.plot(qpos0_hist[:,12])
-plt.show()
+# plt.plot(qpos0_hist[:,2])
+# plt.show()
 # ipdb.set_trace()
---
+++ code/quad_gym_v2.py
@@ -0,0 +1,105 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+from gym import spaces
+import ipdb
+
+class LaikagoEnv2(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        mujoco_env.MujocoEnv.__init__(self, 'laikago2.xml', 2)
+        utils.EzPickle.__init__(self)
+
+        self.action_space = spaces.Box(low=np.ones(12)*-1, high=np.ones(12)*1, dtype=np.float16)
+
+        # low_obs = np.ones(109)*-100
+        # high_obs = np.ones(109)*100
+
+        low_obs = np.ones(137)*-100
+        high_obs = np.ones(137)*100        
+
+        self.observation_space = spaces.Box(low=low_obs, high=high_obs)
+
+
+        self.reward_range = (0, 2000)
+
+    
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+    
+        reward_ctrl =  -1*5e-3  * np.square(action).sum()
+        
+
+        action2 = np.array([action[0], -8+2*action[1], 4.5*action[2]+2, action[3], -8+2*action[4], 4.5*action[5]+2, action[6], 8-2*action[7], 4.5*action[8]+2, action[9], 8-2*action[10], 4.5*action[11]+2])
+
+
+        action=action2
+        
+        self.do_simulation(action[:], self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+        ob = self._get_obs()
+        reward_run = (xposafter - xposbefore)/self.dt
+
+        
+        reward_dist = 0*10*(self.sim.data.qpos[0] - self.init_qpos[0])
+        reward_ht = -1*0*((self.sim.data.qpos[2] + 0.125)/0.125)**2
+
+        reward_eplength = 0.2*2.5 + 0*self.sim.data.time
+
+        # if(self.sim.data.sensordata[71]>0.95):
+        #     reward_pitch = 0.5*(self.sim.data.sensordata[63]) #rewarding for staying upright
+        # else:
+        #     reward_pitch = -.5
+
+
+        reward_pitch = 0.5*.1*10*((self.sim.data.sensordata[83])**16)
+
+        if(self.sim.data.sensordata[63]>0.96):
+            reward_yaw = 0.5*.1*10*((self.sim.data.sensordata[91])**22) #rewarding for heading direction matching x axis (0 yaw)
+        else:
+            reward_yaw = -.5
+
+        reward = reward_ctrl + reward_run + reward_dist + reward_ht + reward_eplength + reward_pitch + reward_yaw
+        done = False
+        # if self.sim.data.qpos[2]<-0.27 or self.sim.data.qpos[2]> 0.001:
+        #     done=True
+        #     reward = -100
+        
+        #frame quaternion gives pitch and roll penalty    
+        
+        # if np.abs(self.sim.data.sensordata[67])<0.94:
+        #     done=True
+        #     reward=reward-10
+        #     # reward = -100
+
+        if np.abs(self.sim.data.sensordata[87])<0.94:
+            done=True
+            reward=reward-10
+            # reward = -100    
+        return ob, reward, done, dict(reward_run=reward_run, reward_ctrl=reward_ctrl, reward_dist=reward_dist, z=self.sim.data.qpos[2])
+
+    def reset(self):
+        qpos = self.init_qpos# + self.np_random.uniform(low=-.1, high=.1, size=self.model.nq) #randomized start state
+        qvel = self.init_qvel #+ self.np_random.randn(self.model.nv) * .1 #randomized start state
+        self.set_state(qpos, qvel)
+
+        obs = self.sim.data.sensordata
+        qposlimit=15; qvellimit=4; anglelimit = 3.14; omegalimit=10; vellimit=10; acclimit=20; torqlimit=100; flimit=100000; 
+
+        return np.concatenate([self.sim.data.qpos.flat, self.sim.data.qvel.flat, obs])
+    
+    def _get_obs(self):
+        # return np.concatenate([
+        #     self.sim.data.qpos.flat[1:],
+        #     self.sim.data.qvel.flat,
+        # ])
+
+        obs = self.sim.data.sensordata
+        qposlimit=15; qvellimit=4; anglelimit = 6.28; omegalimit=10; vellimit=10; acclimit=20; torqlimit=100; flimit=100000; 
+
+        return np.concatenate([self.sim.data.qpos.flat, self.sim.data.qvel.flat, obs])
+
+    def viewer_setup(self):
+            self.viewer.cam.distance = self.model.stat.extent * 0.5
+    
+    def close(self):
+        pass
